f92c8b870f547ab9ddf3675f7273b2b701e91af8
rede3103 <123166042+rede3103@users.noreply.github.com>
2023-03-10 10:50:00 +0100

    Update Abstract_marziano.java

--- src/Abstract_marziano.java
+++ src/Abstract_marziano.java
@@ -1,4 +1,13 @@
-
-public class Abstract_marziano {
+public abstract class Abstract_marziano {
+    protected String matricola;
+	protected int autonomia;
+	protected int ai;
+	protected int tecnologia;
+	public Abstract_marziano(String matricola, int autonomia, int ai, int tecnologia) {
+		this.matricola = "indefinito";
+		this.autonomia = 50;
+		this.ai = 5;
+		this.tecnologia = 5;
+		}
 
 }
