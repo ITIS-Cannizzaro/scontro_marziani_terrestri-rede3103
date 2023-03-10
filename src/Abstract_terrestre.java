c8d389be0fa10a817366feae83399d66521620ec
rede3103 <123166042+rede3103@users.noreply.github.com>
2023-03-10 10:50:19 +0100

    Update Abstract_terrestre.java

--- src/Abstract_terrestre.java
+++ src/Abstract_terrestre.java
@@ -1,4 +1,14 @@
+public abstract class Abstract_terrestre {
+	protected String nome;
+	protected int vita;
+	protected int velocita;
+	protected int armamento;
 
-public class Abstract_terrestre {
+	public Abstract_terrestre(String nome, int vita, int velocita, int armamento) {
+		this.nome="sconosciuto";
+		this.vita=50;
+		this.velocita=5;
+		this.armamento=5;
+		}
 
 }
